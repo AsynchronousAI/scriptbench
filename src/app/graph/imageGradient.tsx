c2cd6f826ae0e8eb649bc170b42076b8be1a89ff
Aarav Sethi <aaravs@Aaravs-Mac-Studio.local>
2025-09-16 18:17:47 -0500

    Greedy meshing

--- src/app/graph/imageGradient.tsx
+++ src/app/graph/imageGradient.tsx
@@ -2,55 +2,81 @@ import { useEffect, useState } from "@rbxts/react";
 import { AssetService } from "@rbxts/services";
 import React from "@rbxts/react";
 
-/* Main */
-const GRADIENT_SIZE = 256;
+const GRADIENT_SIZE = 1024;
 
+/** Gradient Cache */
 const gradientLUT: number[] = table.create(GRADIENT_SIZE);
+
 for (let i = 0; i < GRADIENT_SIZE; i++) {
-  /* linear gradient (0.9 -> 0.1) */
-  gradientLUT[i] = math.floor(
-    (((GRADIENT_SIZE - i - 1) / (GRADIENT_SIZE - 1)) * 0.8 + 0.1) * 255,
-  );
+  const value = math.map(i, 0, GRADIENT_SIZE - 1, 0.1, 0.9);
+  gradientLUT[i] = math.floor(value * 255);
 }
+
+function greedyMesh(
+  resolution: Vector2,
+  y: number,
+  func: (x: number) => number,
+) {
+  /*
+    hello reader! if you are new to greedy meshing,
+    check out this: https://usaco.guide/bronze/intro-greedy?lang=py
+  */
+
+  const meshes: [number, number][] =
+    []; /* first number is start, second is end */
+
+  let prev = 0;
+  let prevWithinBounds = false;
+  for (const x of $range(0, resolution.X)) {
+    if (x < 0 || x >= resolution.X || y < 0 || y >= resolution.Y) continue;
+
+    const maxY = math.floor(func(x / resolution.X) * resolution.Y);
+    const withinBounds = y > maxY;
+
+    if (prevWithinBounds !== withinBounds) {
+      /* x is a turning point */
+      if (!withinBounds) meshes.push([prev, x]);
+      prev = x;
+    }
+    prevWithinBounds = withinBounds;
+  }
+
+  return meshes;
+}
+
 export function EditableImageGradient(props: {
   Color: Color3;
   Function: (x: number) => number;
 }) {
-  /* precompute the color into a partial u32, opacity written later */
-  const r = math.floor(props.Color.R * 255);
-  const g = math.floor(props.Color.G * 255);
-  const b = math.floor(props.Color.B * 255);
-  const colorBits = (b << 16) | (g << 8) | r;
-
   const [image] = useState(
     AssetService.CreateEditableImage({ Size: Vector2.one.mul(GRADIENT_SIZE) }),
   );
 
   useEffect(() => {
     const resolution = image.Size;
-    const imageBuffer = image.ReadPixelsBuffer(Vector2.zero, resolution);
 
-    for (const x of $range(0, resolution.X)) {
-      const maxY = math.floor(props.Function(x / resolution.X) * resolution.Y);
+    for (const y of $range(0, resolution.Y)) {
+      const meshes = greedyMesh(resolution, y, props.Function);
+      const opacity = (gradientLUT[y] ?? 1) / 255;
 
-      for (const y of $range(maxY, resolution.Y)) {
-        if (x < 0 || x >= resolution.X || y < 0 || y >= resolution.Y) continue;
-
-        const opacity = gradientLUT[y];
-        const pixel = colorBits | (opacity << 24);
-        const memoryPos = 4 * (y * resolution.X + x);
-
-        buffer.writeu32(imageBuffer, memoryPos, pixel);
+      for (const mesh of meshes) {
+        image.DrawRectangle(
+          new Vector2(mesh[0], y),
+          new Vector2(mesh[1] - mesh[0], 10),
+          props.Color,
+          opacity,
+          "Overwrite",
+        );
       }
     }
 
-    image.WritePixelsBuffer(Vector2.zero, resolution, imageBuffer);
-
     return () => {
-      image.WritePixelsBuffer(
-        Vector2.zero,
+      image.DrawRectangle(
+        new Vector2(0, 0),
         resolution,
-        buffer.create(buffer.len(imageBuffer)),
+        new Color3(1, 1, 1),
+        1,
+        "Overwrite",
       );
     };
   }, [props.Function]);
